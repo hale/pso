87e9bfa0ce02821cb726cdb145d4258f7f565543
Philip Hale <phil989@gmail.com>
2013-10-15 17:34:47 +0100

    WIP: integration test

---
+++ spec/integration_spec.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+
+module PSO
+  class IntegrationSpec
+
+    describe "this" do
+      it 'passes' do
+      end
+      it 'passes' do
+      end
+      it 'passes' do
+      end
+    end
+  end
+end
